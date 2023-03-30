8dfbc60d8c07e19a1105814aab8a32b24eae3b1c
Tomazini <marcos.tomazini@gmail.com>
2023-03-30 11:23:12 -0300

    first commit

---
+++ src/amplify-config.js
@@ -0,0 +1,14 @@
+/*const amplifyConfig = {
+    "aws_project_region": "MY_PROJECT_REGION",
+    "aws_cognito_region": "MY_USER_POOL_REGION",
+    "aws_user_pools_id": "MY_USER_POOL_ID",
+    "aws_user_pools_web_client_id": "MY_USER_POOL_WEB_CLIENT_ID"
+};*/
+
+const amplifyConfig = {
+    "aws_cognito_region": "sa-east-1",
+    "aws_user_pools_id": "sa-east-1_fAt4WCtp6",
+    "aws_user_pools_web_client_id": "5ilqb1okfdgj120hu1468gc2d9"
+};
+
+export default amplifyConfig;
